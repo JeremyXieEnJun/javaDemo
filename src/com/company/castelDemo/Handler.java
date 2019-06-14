a68748ea9c4c19081d0f83b340605698c582bd06
JeremyXieENJun <1875192015@163.com>
2019-06-14 10:54:43 +0800

    第一次提交

---
+++ src/com/company/castelDemo/Handler.java
@@ -0,0 +1,19 @@
+package com.company.castelDemo;
+
+/**
+ * @author Jeremy
+ * @create 2019/6/12-17:07
+ */
+public class Handler {
+    protected Game game;
+    public Handler(Game game){
+        this.game = game;
+    }
+    public  void doCmd(String word){
+
+     }
+     public boolean isBye(){
+       return false;
+     }
+
+}
